7fe6ea07190d5ab2ed67ef3a3abbe7573371a8af
TioStitch <guialegomes642@gmail.com>
2024-02-07 02:37:08 -0300

    DataSaver - Update #1

--- src/org/tiostitch/datasaver/SplitedSave.java
+++ src/org/tiostitch/datasaver/SplitedSave.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public final class SplitedSave {
 
-    private SplitedSave() throws IOException {
+    private SplitedSave() {
 
         final PlayerData playerData = new PlayerData();
         playerData.setName("User");
@@ -12,30 +12,31 @@ public final class SplitedSave {
         playerData.setGems(250);
 
         final File file = new File("separatedsplit-data.txt");
-        if (!file.exists()) {
-            file.createNewFile();
-        }
 
-        try (Writer writer = new FileWriter(file)) {
-            try (BufferedWriter bF = new BufferedWriter(writer)) {
+        writeSplitObject(file, playerData);
+        loadSplitObject(file);
+    }
 
-                bF.write(splittedPlayerSave(playerData));
-
-                writer.flush();
-                bF.flush();
-            }
-        }
-
+    private void loadSplitObject(File file) {
         final PlayerData newPlayerData = new PlayerData();
-        try (Reader reader = new FileReader(file)) {
-            try (BufferedReader bR = new BufferedReader(reader)) {
-                final String[] splittedbar = bR.readLine().split(":");
-                newPlayerData.setName(splittedbar[0]);
-                newPlayerData.setCoins(Integer.parseInt(splittedbar[1]));
-                newPlayerData.setGems(Integer.parseInt(splittedbar[2]));
+        try (BufferedReader bR = new BufferedReader(new FileReader(file))) {
+            final String[] splittedbar = bR.readLine().split(":");
+            newPlayerData.setName(splittedbar[0]);
+            newPlayerData.setCoins(Integer.parseInt(splittedbar[1]));
+            newPlayerData.setGems(Integer.parseInt(splittedbar[2]));
 
-                printar(newPlayerData);
-            }
+            printar(newPlayerData);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writeSplitObject(File file, PlayerData playerData) {
+        try (BufferedWriter bF = new BufferedWriter(new FileWriter(file))) {
+            bF.write(splittedPlayerSave(playerData));
+            bF.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
